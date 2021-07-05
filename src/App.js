aa1fc40e48974c0691586464dc264c9fb8bb771d
Jatinut Satsara <you@example.com>
2021-07-05 11:23:50 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+const word = "Hello";
 function App() {
-   return (
-     <div>
-        <CharacterCard/>
-     </div>
+ return (
+ <div>
+ {
+ Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+ }
+ </div>
  );
 }
-export default App;
